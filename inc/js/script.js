c636e3f740385f1657602736d1a440d1f73c3358
manojradeshan <manojradeshan2023@gmail.com>
2023-09-05 17:04:37 +0530

    Back to top button

--- inc/js/script.js
+++ inc/js/script.js
@@ -3,6 +3,29 @@ let changeIcon = function (icon) {
   icon.classList.toggle("fa-times");
 };
 
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // copyright text year
 var getYear = new Date().getFullYear();
 document.getElementById("getYear").innerHTML = getYear;
